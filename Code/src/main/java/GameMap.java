ac1f12ec38225c10c550171bddf5d1aab62da734
sibeiz <sibeiz@sfu.ca>
2022-03-19 13:56:25 -0700

    Improve GameMap's 3D (completed)

--- Code/src/main/java/GameMap.java
+++ Code/src/main/java/GameMap.java
@@ -145,7 +145,39 @@ public class GameMap {
     }
 
     private int[][] setNewBoard(int[][] map) {
-
+        int newMap[][] = new int[24][32];
+        for(int i = 0; i < 24; i++){
+            for (int j = 0; j < 32; j++) {
+                if(map[i][j] == 0){
+                    // It is an earth
+                    newMap[i][j] = 0;
+                    if((i!=0) && (map[i-1][j]==1)){
+                        // There are upside wall
+                        newMap[i][j] = 3;
+                        if((j!=0) && (map[i][j-1]==1)){
+                            // There are upside wall and leftist wall
+                            newMap[i][j] = 5;
+                        }
+                    }
+                    else if((j!=0) && (map[i][j-1]==1)){
+                        // There are leftist wall
+                        newMap[i][j] = 4;
+                    }
+                    else if((i!=0) && (j!=0) && (map[i-1][j-1]==1)){
+                        // There are upLeftSide wall
+                        newMap[i][j] = 6;
+                    }
+                }
+                else {
+                    // It is a wall
+                    newMap[i][j] = 1;
+                    if((i!=23) && (map[i+1][j]==0)){
+                        newMap[i][j] = 2;
+                    }
+                }
+            }
+        }
+        return newMap;
     }
 
     /**
@@ -189,7 +221,7 @@ public class GameMap {
             tile[0].setCollision(false);
 
             tile[1] = new Tile();
-            tile[1].tileImg = ImageIO.read(new File("src/main/java/picture/Tiles/wall.png"));
+            tile[1].tileImg = ImageIO.read(new File("src/main/java/picture/Tiles/wall_dark.png"));
             tile[1].setCollision(true);
 
             tile[2] = new Tile();
