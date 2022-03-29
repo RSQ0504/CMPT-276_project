29d7b751c8e138efd76221ec6054793daea84eb4
rqa4 <rqa4@sfu.ca>
2022-03-29 12:41:26 -0700

    Test

---
+++ Code/src/test/java/GameObjectTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameObjectTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void getCollision() {
+  }
+
+  @Test
+  void setCollision() {
+  }
+
+  @Test
+  void getImage() {
+  }
+
+  @Test
+  void getX() {
+  }
+
+  @Test
+  void getY() {
+  }
+
+  @Test
+  void getWidth() {
+  }
+
+  @Test
+  void getHeight() {
+  }
+
+  @Test
+  void setFrame() {
+  }
+}
---
+++ Code/src/test/java/RewardTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RewardTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void setAppear() {
+  }
+
+  @Test
+  void getAppear() {
+  }
+
+  @Test
+  void draw() {
+  }
+
+  @Test
+  void check() {
+  }
+}
