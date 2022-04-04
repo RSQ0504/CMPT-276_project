f6ee4a3a9cfcfa08473842586da38418e08509e8
rqa4 <rqa4@sfu.ca>
2022-04-03 17:17:49 -0700

    test draw

--- Code/src/test/java/RewardTest.java
+++ Code/src/test/java/RewardTest.java
@@ -2,7 +2,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 
@@ -19,7 +24,7 @@ class RewardTest {
     gameFrame = new GameFrame(1,1,1);
     key = null;
     a = new MainCharacter(gameFrame,key);
-    reward = new Food(gameFrame,0,0);
+    reward = new Food(gameFrame,20,20);
   }
 
 
@@ -38,7 +43,32 @@ class RewardTest {
   }
 
   @Test
-  void draw() {
+  void draw() throws InterruptedException {
+    class drawTest extends JPanel {
+      private Frame frame = new Frame("test");
+      public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D)g;
+        reward.draw(g2);
+      }
+
+      public void init() {
+        drawTest Draw = new drawTest();
+        Draw.repaint();
+        Draw.setPreferredSize(new Dimension(20,20));
+        frame.add(Draw);
+        frame.addWindowListener(new WindowAdapter() {
+          @Override
+          public void windowClosing(WindowEvent e) {
+            System.exit(0);
+          }
+        });
+        frame.pack();
+        frame.setVisible(true);
+      }
+    }
+    drawTest test = new drawTest();
+    test.init();
+    Thread.sleep(1000);
   }
 
   @Test
